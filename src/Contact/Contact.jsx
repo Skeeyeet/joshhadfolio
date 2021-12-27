4e93a7ab24b4a4c323819d6cc2f1bb3e8be7d41e
Skeeyeet <75297444+Skeeyeet@users.noreply.github.com>
2021-12-27 20:29:12 +1100

    Contact update

--- src/Contact/Contact.jsx
+++ src/Contact/Contact.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import NormalTitleText from '../Global/NormalTitleText'
+import {AiOutlineMail,AiFillLinkedin,AiFillGithub} from '../../node_modules/react-icons/ai'
 function Contact(){
 return(
     <div className="container-fluid" id="contact">
-        <NormalTitleText Content="Contact me at josh.adomeit@gmail.com"/>
+        <div className='row justify-content-center'>
+            <div className='col-4'>
+            <AiFillGithub className='display-6 text-white'/>
+            <a className='text-white lead ps-2' href='https://github.com/Skeeyeet'>https://github.com/Skeeyeet</a>
+            </div>
+        </div>
+        <div className='row justify-content-center'>
+            <div className='col-4'>
+            <AiFillLinkedin className='display-6 text-white'/>
+            <a className='text-white lead ps-2'>https://www.linkedin.com/in/joshua-hardi-adomeit-344a95181/</a>
+            </div>
+        </div>
+        <div className='row justify-content-center'>
+            <div className='col-4'>
+            <AiOutlineMail className='display-6 text-white'/>
+            <a className='text-white lead ps-2'>josh.adomeit@gmail.com</a>
+            </div>
+        </div>
     </div>
 )
 }
