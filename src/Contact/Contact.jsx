c508ebabce4aaababfdc0f10decdbe1d12b293f8
Skeeyeet <75297444+Skeeyeet@users.noreply.github.com>
2022-01-20 20:27:59 +1100

    Update Contact.jsx

--- src/Contact/Contact.jsx
+++ src/Contact/Contact.jsx
@@ -5,19 +5,19 @@ function Contact(){
 return(
     <div className="container-fluid" id="contact">
         <div className='row justify-content-center'>
-            <div className='col-4'>
+            <div className='col-12 col col-xxl-4'>
             <AiFillGithub className='display-6 text-white'/>
             <a className='text-white lead ps-2' href='https://github.com/Skeeyeet'>https://github.com/Skeeyeet</a>
             </div>
         </div>
         <div className='row justify-content-center'>
-            <div className='col-4'>
+            <div className='col-12 col col-xxl-4'>
             <AiFillLinkedin className='display-6 text-white'/>
             <a className='text-white lead ps-2'>https://www.linkedin.com/in/joshua-hardi-adomeit-344a95181/</a>
             </div>
         </div>
         <div className='row justify-content-center'>
-            <div className='col-4'>
+            <div className='col-12 col col-xxl-4'>
             <AiOutlineMail className='display-6 text-white'/>
             <a className='text-white lead ps-2'>josh.adomeit@gmail.com</a>
             </div>
